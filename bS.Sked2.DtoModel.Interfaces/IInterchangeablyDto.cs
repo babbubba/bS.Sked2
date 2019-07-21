b0f99bd78aed28e9bce19847b1cf92650e8edb75
Fabio Cavallari <f.cavallari@qonsult.it>
2019-07-21 18:41:56 +0200

    Create InterchangeablyBase and TableObject fist implementation.

---
+++ bS.Sked2.DtoModel.Interfaces/IInterchangeablyDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bS.Sked2.DtoModel.Interfaces
+{
+   public interface IInterchangeablyDto<T>
+    {
+        bool IsValid { get; set; }
+        bool IsInit { get; set; }
+
+        void Init();
+        T Get();
+        void Set(T value);
+
+    }
+}
---
+++ bS.Sked2.DtoModel/Interchangeably/TableObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace bS.Sked2.DtoModel.Interchangeably
+{
+    public class TableObject : InterchangeablyBase<DataTable>
+    {
+        DataTable _table { get; set; }
+
+        public override DataTable Get()
+        {
+            ThrowIfNotInit();
+            ThrowIfNotValid();
+            return _table;
+        }
+
+        public override void Init()
+        {
+            _table = new DataTable("table");
+            IsInit = true;
+        }
+
+        public override void Set(DataTable value)
+        {
+            ThrowIfNotInit();
+            _table = value;
+            IsValid = true;
+        }
+
+        public IEnumerable<string> GetHeaders()
+        {
+            ThrowIfNotInit();
+            ThrowIfNotValid();
+            return from DataColumn c in _table.Columns
+                   select c.ColumnName;
+        }
+
+        public IEnumerable<object> GetRow(int rowNumber)
+        {
+            ThrowIfNotInit();
+            ThrowIfNotValid();
+            if (_table.Rows.Count > rowNumber) throw new ApplicationException("Specified row number is out of table rows range.");
+            foreach (DataRow row in _table.Rows)
+            {
+                if (_table.Rows.IndexOf(row) == rowNumber)
+                { return row.ItemArray; }
+            }
+            throw new ApplicationException("No row found at specified row number.");
+        }
+
+        public int GetRowsCount()
+        {
+            return _table.Rows.Count;
+        }
+    }
+}
