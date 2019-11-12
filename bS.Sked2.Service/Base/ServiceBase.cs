2c1c9c3e4b294d9dd1f2bbbba91c4921c5471250
Fabio Cavallari <f.cavallari@qonsult.it>
2019-11-12 15:11:00 +0100

    Added comment to StorageService.

--- bS.Sked2.Service/Base/ServiceBase.cs
+++ bS.Sked2.Service/Base/ServiceBase.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace bS.Sked2.Service.Base
 {
+    /// <summary>
+    /// This is the based service class. Every Services have to implement this abstracted class.
+    /// </summary>
+    /// <seealso cref="bS.Sked2.Structure.Service.IService" />
     public abstract class ServiceBase : Structure.Service.IService
     {
         protected readonly ILogger logger;
--- bS.Sked2.Service/Storage/StorageService.cs
+++ bS.Sked2.Service/Storage/StorageService.cs
@@ -11,18 +11,41 @@ using Zio.FileSystems;
 
 namespace bS.Sked2.Service.Storage
 {
-    public class StorageService : Base.ServiceBase, IStorageService
+    /// <summary>
+    /// This handle file management in a cross plattform virtual file sistem. This implement IDsiposable pattern so remeber to release resources at the end.
+    /// </summary>
+    /// <seealso cref="bS.Sked2.Service.Base.ServiceBase" />
+    /// <seealso cref="bS.Sked2.Structure.Service.IStorageService" />
+    /// <seealso cref="System.IDisposable" />
+    public class StorageService : Base.ServiceBase, IStorageService, IDisposable
     {
+        #region Fields
+        /// <summary>
+        /// The configuration
+        /// </summary>
         private readonly IStorageServiceConfig config;
-        //private readonly PhysicalFileSystem fileSystem;
-        private readonly SubFileSystem workSpaceFileSystem;
+        /// <summary>
+        /// The work space file system
+        /// </summary>
+        private readonly SubFileSystem workSpaceFileSystem; 
+        #endregion
 
+        #region C.tor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StorageService"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="config">The configuration.</param>
+        /// <exception cref="StorageException">
+        /// Error initializing storage. Invalid 'RootPath' provided. - 1
+        /// or
+        /// Error initializing storage. {e.Message} - 2
+        /// </exception>
         public StorageService(ILogger<StorageService> logger, IStorageServiceConfig config) : base(logger)
         {
             this.config = config;
 
             // Check if config is valid
-
             if (string.IsNullOrWhiteSpace(this.config.RootPath)) throw new StorageException(logger, $"Error initializing storage. Invalid 'RootPath' provided.", 1);
 
             // Init the virtual file system
@@ -36,8 +59,17 @@ namespace bS.Sked2.Service.Storage
             {
                 throw new StorageException(logger, $"Error initializing storage. {e.Message}", e, 2);
             }
-        }
+        } 
+        #endregion
 
+        #region File Management
+        /// <summary>
+        /// Copies the file from the origin to the target virtual path.
+        /// </summary>
+        /// <param name="source">The origin virtual path.</param>
+        /// <param name="target">The target virtual path.</param>
+        /// <param name="overwrite"></param>
+        /// <exception cref="StorageException">Error coping file from '{source.RealPath}' to '{target.RealPath}'. - 3</exception>
         public void FileCopy(IVirtualPath source, IVirtualPath target, bool overwrite = false)
         {
             try
@@ -50,6 +82,11 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Deletes the specified file.
+        /// </summary>
+        /// <param name="path">The virtual path.</param>
+        /// <exception cref="StorageException">Error deleting file '{path.RealPath}'. - 4</exception>
         public void FileDelete(IVirtualPath path)
         {
             try
@@ -62,6 +99,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Files the exists.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking if file '{path.RealPath}' exists. - 5</exception>
         public bool FileExists(IVirtualPath path)
         {
             try
@@ -74,24 +117,42 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Files the get attributes.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public bool FileGetAttributes(IVirtualPath path)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Files the get creation time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking file '{path.RealPath}'. - 7</exception>
         public DateTime FileGetCreationTime(IVirtualPath path)
         {
-                try
-                {
-                    return workSpaceFileSystem.GetCreationTime(path.RealPath);
+            try
+            {
+                return workSpaceFileSystem.GetCreationTime(path.RealPath);
 
-                }
-                catch (Exception e)
-                {
-                    throw new StorageException(logger, $"Error checking file '{path.RealPath}'.", e, 7);
-                }
+            }
+            catch (Exception e)
+            {
+                throw new StorageException(logger, $"Error checking file '{path.RealPath}'.", e, 7);
+            }
         }
 
+        /// <summary>
+        /// Files the get last access time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking file '{path.RealPath}'. - 8</exception>
         public DateTime FileGetLastAccessTime(IVirtualPath path)
         {
             try
@@ -105,6 +166,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Files the get last write time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking file '{path.RealPath}'. - 9</exception>
         public DateTime FileGetLastWriteTime(IVirtualPath path)
         {
             try
@@ -118,6 +185,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Files the get lenght.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking lenght of file '{path.RealPath}'. - 10</exception>
         public long FileGetLenght(IVirtualPath path)
         {
             try
@@ -131,6 +204,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Moves the file from the origin to the target virtual path.
+        /// </summary>
+        /// <param name="source">The origin virtual path.</param>
+        /// <param name="target">The target virtual path.</param>
+        /// <exception cref="StorageException">Error coping file from '{source.RealPath}' to '{target.RealPath}'. - 11</exception>
         public void FileMove(IVirtualPath source, IVirtualPath target)
         {
             try
@@ -143,6 +222,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Reads the binary file.
+        /// </summary>
+        /// <param name="path">The virtual path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error reading file '{path.RealPath}'. - 12</exception>
         public byte[] FileReadBinary(IVirtualPath path)
         {
             try
@@ -155,6 +240,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Reads the text file.
+        /// </summary>
+        /// <param name="path">The virtual path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error reading file '{path.RealPath}'. - 13</exception>
         public string FileReadString(IVirtualPath path)
         {
             try
@@ -167,6 +258,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Save a bit array in a Binary File
+        /// </summary>
+        /// <param name="binaryContent">Content of the binary.</param>
+        /// <param name="path">The path.</param>
+        /// <exception cref="StorageException">Error writing file '{path.RealPath}'. - 14</exception>
         public void FileSave(byte[] binaryContent, IVirtualPath path)
         {
             try
@@ -179,6 +276,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Save a string in a Text File
+        /// </summary>
+        /// <param name="textContent">Content of the text.</param>
+        /// <param name="path">The path.</param>
+        /// <exception cref="StorageException">Error writing file '{path.RealPath}'. - 15</exception>
         public void FileSave(string textContent, IVirtualPath path)
         {
             try
@@ -191,23 +294,41 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Set the file attributes.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public bool FileSetAttributes(IVirtualPath path)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Set the file creation time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <exception cref="StorageException">Error setting creation date to file '{path.RealPath}'. - 17</exception>
         public void FileSetCreationTime(IVirtualPath path, DateTime dateTime)
         {
-                try
-                {
-                    workSpaceFileSystem.SetCreationTime(path.RealPath, dateTime);
-                }
-                catch (Exception e)
-                {
-                    throw new StorageException(logger, $"Error setting creation date to file '{path.RealPath}'.", e, 17);
-                }
+            try
+            {
+                workSpaceFileSystem.SetCreationTime(path.RealPath, dateTime);
+            }
+            catch (Exception e)
+            {
+                throw new StorageException(logger, $"Error setting creation date to file '{path.RealPath}'.", e, 17);
+            }
         }
 
+        /// <summary>
+        /// Set the file  last access time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <exception cref="StorageException">Error setting last access time to file '{path.RealPath}'. - 18</exception>
         public void FileSetLastAccessTime(IVirtualPath path, DateTime dateTime)
         {
             try
@@ -220,6 +341,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Set the file  last write time.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <exception cref="StorageException">Error setting last write time to file '{path.RealPath}'. - 19</exception>
         public void FileSetLastWriteTime(IVirtualPath path, DateTime dateTime)
         {
             try
@@ -231,7 +358,14 @@ namespace bS.Sked2.Service.Storage
                 throw new StorageException(logger, $"Error setting last write time to file '{path.RealPath}'.", e, 19);
             }
         }
+        #endregion File Management
 
+        #region Folder Management
+        /// <summary>
+        /// Folders the create.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="StorageException">Error creating directory '{path.RealPath}'. - 20</exception>
         public void FolderCreate(IVirtualPath path)
         {
             try
@@ -244,6 +378,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Folders the delete.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="deleteSubElements">if set to <c>true</c> [delete sub elements].</param>
+        /// <exception cref="StorageException">Error deleting folder '{path.RealPath}'. - 21</exception>
         public void FolderDelete(IVirtualPath path, bool deleteSubElements = false)
         {
             try
@@ -256,6 +396,14 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Folders the enumerate paths.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="searchOption">The search option.</param>
+        /// <param name="searchPattern">The search pattern.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking folder '{path.RealPath}'. - 22</exception>
         public IVirtualPath[] FolderEnumeratePaths(IVirtualPath path, System.IO.SearchOption searchOption, string searchPattern = "*")
         {
             try
@@ -268,6 +416,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Folders the exists.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error checking folder '{path.RealPath}'. - 23</exception>
         public bool FolderExists(IVirtualPath path)
         {
             try
@@ -280,6 +434,13 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Folders the move.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        /// <param name="overwrite">if set to <c>true</c> [overwrite].</param>
+        /// <exception cref="StorageException">Error moving folder from '{source.RealPath}' to '{target.RealPath}'. - 24</exception>
         public void FolderMove(IVirtualPath source, IVirtualPath target, bool overwrite = false)
         {
             try
@@ -292,6 +453,12 @@ namespace bS.Sked2.Service.Storage
             }
         }
 
+        /// <summary>
+        /// Watch the folder changes. You have to register the events that the return value implements for handle folder's changes.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="StorageException">Error watching folder '{path.RealPath}'. - 25</exception>
         public IVirtualPathWatch FolderWatch(IVirtualPath path)
         {
             try
@@ -304,5 +471,53 @@ namespace bS.Sked2.Service.Storage
                 throw new StorageException(logger, $"Error watching folder '{path.RealPath}'.", e, 25);
             }
         }
+        #endregion Folder Management
+
+        #region IDisposable Support
+        /// <summary>
+        /// The disposed value
+        /// </summary>
+        private bool disposedValue = false; // Per rilevare chiamate ridondanti
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    workSpaceFileSystem.Dispose();
+                }
+
+                // TODO: liberare risorse non gestite (oggetti non gestiti) ed eseguire sotto l'override di un finalizzatore.
+                // TODO: impostare campi di grandi dimensioni su Null.
+
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: eseguire l'override di un finalizzatore solo se Dispose(bool disposing) include il codice per liberare risorse non gestite.
+        // ~StorageService()
+        // {
+        //   // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+        //   Dispose(false);
+        // }
+
+        // Questo codice viene aggiunto per implementare in modo corretto il criterio Disposable.
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+            Dispose(true);
+            // TODO: rimuovere il commento dalla riga seguente se è stato eseguito l'override del finalizzatore.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
