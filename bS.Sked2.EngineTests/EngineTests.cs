92ece0fb4074b46551fbf4aadd0d09587a3fa96c
Fabio Cavallari <f.cavallari@qonsult.it>
2019-11-15 07:22:48 +0100

    Implemented tests

--- bS.Sked2.EngineTests/EngineTests.cs
+++ bS.Sked2.EngineTests/EngineTests.cs
@@ -13,6 +13,7 @@ using bS.Sked2.Structure.Engine.Data;
 using System.Data;
 using bS.Sked2.Service.Message;
 using bS.Sked2.Extensions.Common.SqlServer;
+using bS.Sked2.Extensions.Common;
 
 namespace bS.Sked2.Engine.Tests
 {
@@ -21,55 +22,78 @@ namespace bS.Sked2.Engine.Tests
     {
         private ILogger<Engine> logger;
         private IMessageService messageService;
+        private Common commonModule;
         private Engine engine;
+        private EngineJob job;
+        private EngineTask task;
 
         [TestInitialize]
         public void Init()
-        { 
-           logger = Mock.Of<ILogger<Engine>>();
-            engine = new Engine(logger);
-        }
-        [TestMethod()]
-        public void ExecuteElementTest()
         {
-            var messageService = new MessageService();
+            logger = Mock.Of<ILogger<Engine>>();
+            engine = new Engine(logger);
 
-
-            var commonModule = new Extensions.Common.Common(logger, messageService);
+            messageService = new MessageService();
+            commonModule = new Common(logger, messageService);
             commonModule.Init();
-
-            var job = new EngineJob(logger, messageService);
+            job = new EngineJob(logger, messageService);
             job.Start();
-
-            var task = new EngineTask(logger, messageService);
+            task = new EngineTask(logger, messageService);
             task.ParentJob = job;
             task.Start();
-
+        }
+        [TestMethod()]
+        public void ExecuteFlatFileReader()
+        {
             var flatFileReader = new FlatFileReader(logger, messageService);
             flatFileReader.ParentModule = commonModule;
             flatFileReader.ParentTask = task;
-            flatFileReader.SetDataValue(EngineDataDirection.Input, "SourceFilePath", new StringValue(@"c:\temp\provincia-regione-sigla.csv"));
+            flatFileReader.SetDataValue(EngineDataDirection.Input, "SourceFilePath", new StringValue(@".\TestDataFiles\provincia-regione-sigla.csv"));
             flatFileReader.SetDataValue(EngineDataDirection.Input, "FirstRowHasHeader", new BoolValue(false));
             flatFileReader.SetDataValue(EngineDataDirection.Input, "ColumnDelimiter", new CharValue(','));
             engine.ExecuteElement(flatFileReader);
             var resultFlatFileReader = flatFileReader.GetDataValue(EngineDataDirection.Output, "Table").Get<DataTable>();
+            Assert.IsNotNull(resultFlatFileReader);
 
-            //            var sqlQueryReader = new SqlQueryReader(logger, messageService);
-            //            sqlQueryReader.ParentModule = commonModule;
-            //            sqlQueryReader.ParentTask = task;
-            //            sqlQueryReader.SetDataValue(EngineDataDirection.Input, "ConnectionString", new StringValue(@"Data Source=(localdb)\mssqllocaldb;Persist Security Info=True;User ID=sa;Password=gabe;Database=EPS_HR;"));
-            //            sqlQueryReader.SetDataValue(EngineDataDirection.Input, "SqlQuery", new StringValue(@"
-            //SELECT [Id]
-            //      ,[Code]
-            //      ,[PayrollingCode]
-            //      ,[Name]
-            //      ,[Description]
-            //      ,[Order]
-            //      ,[Type]
-            //      ,[IsEnabled]
-            //  FROM [BaseActivities];"));
-            //            engine.ExecuteElement(sqlQueryReader);
-            //            var resultSqlQueryReader = sqlQueryReader.GetDataValue(EngineDataDirection.Output, "Table").Get<DataTable>();
+        }
+
+        [TestMethod()]
+        public void ExecuteSqlQueryReader()
+        {
+            var sqlQueryReader = new SqlQueryReader(logger, messageService);
+            sqlQueryReader.ParentModule = commonModule;
+            sqlQueryReader.ParentTask = task;
+            sqlQueryReader.SetDataValue(EngineDataDirection.Input, "ConnectionString", new StringValue(@"Data Source=(localdb)\mssqllocaldb;Persist Security Info=True;User ID=sa;Password=gabe;Database=EPS_HR;"));
+            sqlQueryReader.SetDataValue(EngineDataDirection.Input, "SqlQuery", new StringValue(@"
+            SELECT [Id]
+                  ,[Code]
+                  ,[PayrollingCode]
+                  ,[Name]
+                  ,[Description]
+                  ,[Order]
+                  ,[Type]
+                  ,[IsEnabled]
+              FROM [BaseActivities];"));
+            engine.ExecuteElement(sqlQueryReader);
+            var resultSqlQueryReader = sqlQueryReader.GetDataValue(EngineDataDirection.Output, "Table").Get<DataTable>();
+            Assert.IsNotNull(resultSqlQueryReader);
+        }
+
+        [TestMethod()]
+        public void ExecuteSqlWriter()
+        {
+            var dt = new DataTable("table");
+            dt.Columns.Add("Column1");
+            dt.Columns.Add("Column2");
+            dt.Columns.Add("Column3");
+            for (int i = 1; i < 11; i++)
+            {
+                var r = dt.NewRow();
+                r["Column1"] = $"Col1_Row{i}";
+                r["Column2"] = $"Col2_Row{i}";
+                r["Column3"] = $"Col3_Row{i}";
+                dt.Rows.Add(r);
+            }
 
             var sqlWriter = new SqlWriter(logger, messageService);
             sqlWriter.ParentModule = commonModule;
@@ -81,8 +105,8 @@ namespace bS.Sked2.Engine.Tests
             mappings.AddValue(new DictionaryEntryValue("Column2", "1"));
             mappings.AddValue(new DictionaryEntryValue("Column3", "2"));
             sqlWriter.SetDataValue(EngineDataDirection.Input, "ColumnsMapping", mappings);
-            sqlWriter.SetDataValue(EngineDataDirection.Input, "SourceTable", flatFileReader.GetDataValue(EngineDataDirection.Output, "Table"));
-         
+            sqlWriter.SetDataValue(EngineDataDirection.Input, "SourceTable", new TableValue(dt));
+
             engine.ExecuteElement(sqlWriter);
 
         }
